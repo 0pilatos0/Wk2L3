f492bfc2742d2cd84bb7ae432eff34fd15b6a054
Paul <Paulvanderlei@hotmail.nl>
2019-09-09 14:37:01 +0200

    Poging tot suc6

---
+++ Applicatie.js
@@ -0,0 +1,21 @@
+// Paul van der Lei
+// Media- en Applicatieontwikkelaar leerjaar 1
+
+/*alert("Welkom bij de rekenmachine");
+var tafel = prompt ('van welke tafel wil jij je getallen hebben?', 'tafel nummer');
+var hoeveelheid = prompt ('tot hoever wil jij dat de tafel word berekent', 'aantal')
+
+tafels(tafel, hoeveelheid)
+function tafels(tafel, hoeveelheid) {
+    for (let hoeveelheidT = 0; hoeveelheidT <= hoeveelheid; hoeveelheidT++) {
+        
+        let antwoord = tafel * hoeveelheidT
+       document.write(tafel + ' * ' + hoeveelheidT + ' = ' + antwoord + ' ');
+   }
+}*/
+
+var basisgetal = 12;
+document.write(basisgetal + " ")
+document.write(basisgetal + " ")
+document.write(basisgetal + " ")
+document.write(basisgetal + " ")
